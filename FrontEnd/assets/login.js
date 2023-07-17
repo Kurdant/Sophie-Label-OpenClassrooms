8235f36db1476a31e40603cafceae26c29f642af
Hugo <hugo@shenron.fr>
2023-07-17 16:42:05 +0200

    first commit

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,41 +1,39 @@
 window.addEventListener("load", async () => {
-    // SO form code
     let formulaire = document.getElementById("Formulaire");
     formulaire.addEventListener("submit", (e) => {
       e.preventDefault();
       let email = document.getElementById("email").value;
       let password = document.getElementById("password").value;
-    
+      const errorForm = document.getElementById("error_form");
+  
       if (email === "" || password === "") {
         alert("Veuillez remplir le formulaire.");
       } else {
         fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password
-            }),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
         })
-            .then((response) => {
-                if(response.ok && response.status === 200) {
-                    alert("Bien joué");
-                } else {
-                    alert("Une erreur s'est produite lors de la soumission du formulaire.");
-                }
-            })
-            .catch((error) => {
-                console.error(error);
-                alert("Une erreur s'est produite lors de la soumission du formulaire.");
-            });
+          .then((response) => {
+            if (response.ok && response.status === 200) {
+                window.location.href = "http://127.0.0.1:5500/FrontEnd/index.html";
+            } else {
+                errorForm.classList.add("visible");
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+            alert("Une erreur s'est produite lors de la soumission du formulaire.");
+          });
       }
-    
+  
       document.getElementById("email").value = "";
       document.getElementById("password").value = "";
-    });  
-});
-  
-  
+    });
+  });
   
